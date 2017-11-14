b154f58e097eafd70a687cab807b65a5cb78aed2
Jared David <jwidavid72@gmail.com>
2017-11-13 20:26:12 -0500

    Initial version of the token.

--- turtle_token.sol
+++ turtle_token.sol
@@ -1,137 +1,158 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.16;
+
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
 
 contract TurtleToken {
-    
-    /* Array w/ balances of every address */
-    mapping(address => uint256) balances;
-    
-    /* Size of balance mapping */
-    uint256 numBalances = 0;
-    
-    /*  Owner gives permission to spender to spend a specified 
-        amount of BubbleCoin. The value located at 
-        approved[owner][spender] is the amount that spender is allowed
-        to spend. */
-    mapping(address => mapping(address => uint)) approved;
+    // Public variables of the token
+    string public name;
+    string public symbol;
+    uint8 public decimals = 0; // 18 decimals is the strongly suggested default, avoid changing it
+    uint256 public totalSupply;
 
-    /* Notify user when a transfer has occurred */
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-    /* Notify user when allowance request is approved */
-    event Approval(address indexed _owner, 
-                   address indexed _spender, uint256 _value);
-    
-    /* total supply of tokens */
-    uint supply;
-    
-    /* Publically displayed name (Turtle Token) & symbol (TT) */
-    bytes32 public constant name = "BubbleCoin";
-    bytes32 public constant symbol = "BUBL";
+    // This creates an array with all balances
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping (address => uint256)) public allowance;
 
-    /* Amount of decimal places associated with  */
-    uint public constant decimals = 18;
-    
-    /* Number of payout periods remaining in contract (24 in total) */
-    uint payoutsLeft = 24;
-    
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////// ERC20 Required Functions ////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
 
-    /* Returns the total supply of BubbleCoins in circulation */
-    function totalSupply() 
-    external constant returns (uint totSupply) {
-        return supply;
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    // This notifies clients about the amount burnt
+    event Burn(address indexed from, uint256 value);
+
+
+    /**
+     * Constrctor function
+     *
+     * Initializes contract with initial supply tokens to the creator of the contract
+     */
+    function TurtleToken(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+    ) public {
+        balanceOf[msg.sender] = totalSupply;    // Give the creator all initial tokens
+        name = tokenName;                       // Set the name for display purposes
+        symbol = tokenSymbol;                   // Set the symbol for display purposes
     }
 
-    /* Returns the BubbleCoin balance of specified address */
-    function balanceOf(address _owner) 
-    external constant returns (uint chkBalance) {
-        return balances[_owner];
+
+    /**
+     * Internal transfer, only can be called by this contract
+     */
+    function _transfer(address _from, address _to, uint _value) internal {
+        // Prevent transfer to 0x0 address. Use burn() instead
+        require(_to != 0x0);
+        // Check if the sender has enough
+        require(balanceOf[_from] >= _value);
+        // Check for overflows
+        require(balanceOf[_to] + _value > balanceOf[_to]);
+        // Save this for an assertion in the future
+        uint previousBalances = balanceOf[_from] + balanceOf[_to];
+        // Subtract from the sender
+        balanceOf[_from] -= _value;
+        // Add the same to the recipient
+        balanceOf[_to] += _value;
+        Transfer(_from, _to, _value);
+        // Asserts are used to use static analysis to find bugs in your code. They should never fail
+        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);
     }
 
-    /*  Transfers specified amount of BubbleCoin (_value) from 
-        address that is calling the contract to a specified 
-        address (_to) */
-    function transfer(address _to, uint256 _value) 
-    external returns (bool success) {
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
+
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transfer(address _to, uint256 _value) public {
+        _transfer(msg.sender, _to, _value);
+    }
+
+
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` in behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
+
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens in your behalf
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     */
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+
+
+    /**
+     * Set allowance for other address and notify
+     *
+     * Allows `_spender` to spend no more than `_value` tokens in your behalf, and then ping the contract about it
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     * @param _extraData some extra information to send to the approved contract
+     */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
             return true;
-        } else {
-            // failed transaction
-            return false;
         }
     }
 
-    /*  Transfers specified amount of BubbleCoin (_value) from one 
-        address (_from) to another (_to) */
-    function transferFrom(address _from, address _to, uint256 _value)             
-    external returns (bool success) {
-        if (balances[_from] >= _value &&
-           approved[_from][msg.sender] >= _value &&
-           _value > 0) {
-                
-            balances[_from] -= _value;
-            approved[_from][msg.sender] -= _value;
-            balances[_to] += _value;
-            
-            return true;
-        } else {
-            // failed transaction
-            return false;
-        }
+
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     */
+    function burn(uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        Burn(msg.sender, _value);
+        return true;
     }
 
-    /* Allows the _spender address to take money out of the msg.sender's
-       account up until the specified _value amount */
-    function approve(address _spender, uint256 _value) 
-    external returns (bool success) {
-        if (balances[msg.sender] >= _value) {
-            approved[msg.sender][_spender] = _value;
-            return true;    
-        }
-        return false;
-    }
 
-    /* Returns the current allowance given to one account */
-    function allowance(address _owner, address _spender) 
-    external constant returns (uint remaining) {
-        return approved[_owner][_spender];
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     */
+    function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        Burn(_from, _value);
+        return true;
     }
-
-///////////////////////////////////////////////////////////////////////////////
-///////////////////////////// Personal Functions //////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-    
-    /*  Mints new BubbleCoins & adds them to the creator's address
-        as well as the total supply in circulation */
-    function mint(uint256 numberOfCoins) 
-    external {
-        balances[msg.sender] += numberOfCoins;
-        supply += numberOfCoins;
-    }
-    
-    /* Returns the balance of the address that is calling the contract */
-    function getMyBalance() 
-    external constant returns (uint256 yourBalance) {
-        return balances[msg.sender];
-    }
-    
-/*  Formula for determining token payout per paycheck:
-    
-    # current employees (balances.length equivalent) *
-    # payouts remaining in year (payoutsLeft) * 
-    # coins paid out per employee this period, rounded down (payoutAmt)
-    --------------------------------
-    # coins remaining in pool (balances[msg.sender])
-
-    **After each pay period: 
-    
-    updated # coins remaining = 
-        # coins remaining - (# employees * # coins/employee paid)
-        
-*/
-
 }
